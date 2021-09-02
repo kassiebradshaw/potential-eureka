bc110f885d5bc65e7be58650bc43d6dd38d0929b
Kassie Bradshaw <kassie.r.bradshaw@gmail.com>
2021-09-01 21:20:03 -0700

    Fixed Allowed_Host in settings.py

--- config/settings.py
+++ config/settings.py
@@ -18,8 +18,8 @@ SECRET_KEY = env.str('SECRET_KEY')
 # https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = env.bool('DEBUG')
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
-# ALLOWED_HOSTS = tuple(env.list('ALLOWED_HOSTS'))
-ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
+ALLOWED_HOST = tuple(env.list('ALLOWED_HOSTS'))
+# ALLOWED_HOSTS = ['localhost', '0.0.0.0', '127.0.0.1']
 
 # APPS
 # ------------------------------------------------------------------------------
